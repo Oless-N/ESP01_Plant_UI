b7e662790ba0bc9fac6b340ab53308f3908e0049
oless nechytailo <korklal@gmail.com>
2021-01-17 14:21:16 +0200

    start prj

--- Makefile
+++ Makefile
@@ -1,16 +1,71 @@
-get_CH340_mac:
-	git clone https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver
-micropython:
-	git clone -b v1.13 --single-branch https://github.com/micropython/micropython.git
+#####################################################################
+## --chip {auto, esp8266, esp32}]                                  ##
+## --port PORT                                                     ##
+## --baud BAUD                                                     ##
+## --before {default_reset, no_reset, no_reset_no_sync}            ##
+## --after {hard_reset, soft_reset, no_reset}                      ##
+## --no-stub                                                       ##
+## --trace                                                         ##
+## --override-vddsdio [{1.8V, 1.9V, OFF}]                          ##
+#####################################################################
+## load_ram, dump_mem, read_mem, write_mem, write_flash, run,      ##
+## image_info, make_image, elf2image, read_mac, chip_id, flash_id, ##
+## read_flash_status, write_flash_status, read_flash,              ##
+## verify_flash, erase_flash, erase_region, version                ##
+#####################################################################
+
+PORT = /dev/ttyUSB0
+#PORT = /dev/ttyUSB1
+
+BAUDRATE = 115200
+#BAUDRATE = 57600
+#BAUDRATE = 9600
+
+#CHIP = esp8266
+CHIP = esp32
+
+#FIRMWARE = esp8266-1m-20200902-v1.13.bin
+FIRMWARE = esp32-idf3-20200902-v1.13.bin
+
+setup_dev:
+	sudo apt-get install esptool
+	pip install -r requirements.txt
+
+check_esp:
+	#sudo esptool --chip $(CHIP) --port $(PORT) --baud $(BAUDRATE) --before default_reset --after hard_reset flash_id
+	esptool --port $(PORT) --before default_reset --baud $(BAUDRATE) --after hard_reset flash_id
+	esptool --port $(PORT) --before de4fault_reset --baud $(BAUDRATE) --after hard_reset flash_id
+
 ports:
-	dmesg | grep ttyS
+	dmesg | grep tty
+
+permission:
+	sudo chmod +x $(PORT)
+	sudo chmod +x venv/lib/python3.8/site-packages/esptool.py
+
+read_flash:
+	esptool --chip $(CHIP) --port $(PORT) --baud $(BAUDRATE) read_flash_status
 
 # Prepare for micropiton
 micropython_bin: # 1M of flash # other bin's: http://micropython.org/download/esp8266/
-	wget http://micropython.org/resources/firmware/esp8266-1m-20210116-unstable-v1.13-268-gf7aafc062.bin
+	wget http://micropython.org/resources/firmware/$(FIRMWARE)
+
 clear_esp:
-	esptool.py --port /dev/ttyUSB0 erase_flash
+	esptool.py --chip $(CHIP) --port $(PORT) --baud $(BAUDRATE) erase_flash
+
 burn_micro:	#default baud rate 115200
-	esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash --flash_size=detect 0 esp8266-1m-20210116-unstable-v1.13-268-gf7aafc062.bin
+	esptool.py --chip $(CHIP) --port $(PORT) --baud $(BAUDRATE) write_flash --flash_size=detect 0 $(FIRMWARE)
+
 burn_micro_autoport:
 	esptool.py --port /dev/$(ls /dev | grep cu.wchusbserial) erase_flash
+
+
+#CH340 drivers
+get_CH340_lin:
+	wget https://sparks.gogo.co.nz/assets/_site_/downloads/CH340_LINUX.zip
+
+get_CH340_win:
+	wget https://sparks.gogo.co.nz/assets/_site_/downloads/CH34x_Install_Windows_v3_4.zip
+
+get_CH340_mac:
+	wget https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/blob/master/CH34x_Install_V1.5.pkg
--- main.py
+++ main.py
@@ -1,16 +1,82 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Complete project details at https://RandomNerdTutorials.com
+from micropython.ports.teensy.memzip_files.main import led
+from micropython.tests.extmod.ussl_basic import socket
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def web_page():
+    if led.value() == 1:
+        gpio_state = "ON"
+    else:
+        gpio_state = "OFF"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    html = """
+    <html>
+        <head> 
+            <title>ESP Web Server</title> 
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" href="data:,"> 
+                <style>
+                    html{
+                        font-family: Helvetica; 
+                        display:inline-block; 
+                        margin: 0px auto; 
+                        text-align: center;
+                    }
+                    h1{
+                        color: #0F3376; 
+                        padding: 2vh;
+                        }
+                    p{font-size: 1.5rem;}
+                    .button{
+                        display: inline-block; 
+                        background-color: 
+                        #e7bd3b; 
+                        border: none; 
+                        border-radius: 4px; 
+                        color: white; 
+                        padding: 16px 40px; 
+                        text-decoration: none; 
+                        font-size: 30px; 
+                        margin: 2px; 
+                        cursor: pointer;
+                        }
+                    .button2{
+                        background-color: #4286f4;
+                    }
+                </style>
+                </head>
+                <body> 
+                <h1>ESP Web Server</h1> 
+                    <p>GPIO state: <strong>""" + gpio_state + """</strong></p>
+                    <p><a href="/?led=on"><button class="button">ON</button></a></p>
+                    <p><a href="/?led=off"><button class="button button2">OFF</button></a></p>
+                </body>
+    </html>
+    """
+    return html
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+    conn, addr = s.accept()
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)
+    led_on = request.find('/?led=on')
+    led_off = request.find('/?led=off')
+    if led_on == 6:
+        print('LED ON')
+        led.value(1)
+    if led_off == 6:
+        print('LED OFF')
+        led.value(0)
+    response = web_page()
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
