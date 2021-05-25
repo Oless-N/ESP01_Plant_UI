8a49b2400a3b670cfaaee7f9b5cd9d49b605d44e
Nechytailo Oleksii <korklal@gmail.com>
2021-05-25 14:29:24 +0300

    update ui handlers

---
+++ app/test_fake_data.py
@@ -0,0 +1,24 @@
+FAKE_SENSOR_DATA = {
+    'temp_0': 10,
+    'temp_1': 11,
+    'temp_2': 12,
+    'temp_3': 11,
+    'hidro_0': 50,
+    'hidro_1': 55,
+    'hidro_2': 54,
+    'hidro_3': 56,
+    'pressure_0': 728,
+    'time': 1523423,
+}
+
+FAKE_DEVICES_STATUS = {
+    'relay_0': True,
+    'relay_1': True,
+    'relay_2': False,
+    'relay_3': True,
+    'relay_4': False,
+    'relay_5': True,
+    'relay_6': False,
+    'relay_7': True,
+    'motor': 230
+}
