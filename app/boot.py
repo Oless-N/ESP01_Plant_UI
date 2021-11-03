04d4304dfddaf5e49b90bbc9ff89a981c5d6cdcf
Nechytailo Oleksii <korklal@gmail.com>
2021-11-03 10:41:56 +0200

    update boot

--- app/boot.py
+++ app/boot.py
@@ -1,29 +1,38 @@
 try:
-  import usocket as socket
+    import usocket as socket
 except:
-  import socket
+    import socket
 
-from main import main
-# import network
+from time import sleep
+
+from machine import Pin, I2C
+import network
+
 import esp
-import gc
-gc.collect()
+
 esp.osdebug(None)
 
-#
-# ssid = 'Plant_01'
-# password = 'yourplant'
-#
-# station = network.WLAN(network.STA_IF)
-#
-# station.active(True)
-# station.connect(ssid, password)
-#
-# while station.isconnected() == False:
-#   pass
-#
-# print('Connection successful')
-# print(station.ifconfig())
+import gc
 
 gc.collect()
-main()
+
+import BME280
+
+# ESP32 - Pin assignment
+i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
+# ESP8266 - Pin assignment
+# i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
+
+ssid = 'REPLACE_WITH_YOUR_SSID'
+password = 'REPLACE_WITH_YOUR_PASSWORD'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+    pass
+
+print('Connection successful')
+print(station.ifconfig())
